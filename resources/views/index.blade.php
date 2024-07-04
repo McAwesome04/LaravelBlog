1640d3787879811808cadb7f573f32932b7ef04e
BadHunterN1 <ahmam2004@gmail.com>
2024-07-04 23:39:19 +0300

    ay 7aga

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Render All Elements Normally -->
+    <link rel="stylesheet" href="../css/normalize.css">
+    <!-- Font Awesome Library -->
+    <link rel="stylesheet" href="../css/all.min.css">
+    <!-- Main Template CSS File -->
+    <link rel="stylesheet" href="../css/test.css">
+    <!-- Google Fonts -->
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-wrap">
+                <div class="logo"><img src="imgs/images.png" alt=""></div>
+                <div class="search-wrap">
+                    <i class="fa-solid fa-magnifying-glass-plus"></i>
+                    <input type="text" placeholder="Search..">
+                </div>
+            </div>
+            <div class="register">
+                <a href="#" class="log-in">Log In</a>
+                <a href="#" class="Create-account">Create account</a>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
