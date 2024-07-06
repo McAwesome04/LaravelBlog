783927a22e182c7cb9e5dfc6d0e8c6a388ea0952
BadHunterN1 <ahmam2004@gmail.com>
2024-07-06 21:56:25 +0300

    ay comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,8 +13,8 @@
     <link rel="stylesheet" href="{{ asset('css/test.css') }}">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 
@@ -26,6 +26,49 @@
                     <div class="msidebar flex">
                         <div class="scontainer flex">
                             <label for="menu-toggle" class="menu-icon"><i class="fa-solid fa-x"></i></label>
+                            <div class="sections flex">
+                <div class="welcome flex">
+                    <h3>This Blog has an unknown community of <span> 1 </span> amazing people like you</h3>
+                    <p>We're a place where folk share, stay up-to-date and grow their careers.</p>
+                    <div class="register flex">
+                        <a href="#" class="log-in">Log In</a>
+                        <a href="#" class="Create-account">Create account</a>
+                    </div>
+                </div>
+                <div class="sub-sections flex">
+                    <a href="#"><i class="fa-solid fa-house"></i> Home</a>
+                    <a href="#"><i class="fa-solid fa-microphone-lines"></i> Podcast</a>
+                    <a href="#"><i class="fa-solid fa-video"></i> Videos</a>
+                    <a href="#"><i class="fa-solid fa-tags"></i> Tags</a>
+                    <a href="#"><i class="fa-regular fa-lightbulb"></i> Help</a>
+                    <a href="#"><i class="fa-solid fa-basket-shopping"></i> Shop</a>
+                    <a href="#"><i class="fa-solid fa-heart"  style="color: rgb(255, 0, 0);"></i> AD</a>
+                    <a href="#" style="display: flex; align-items: center; gap: 4px" ><img src="{{ asset('img/images.png') }}" style="width: 20px;" alt="">About</a>
+                    <a href="#"><i class="fa-solid fa-headphones"></i> Contact</a>
+                    <a href="#"><i class="fa-solid fa-book-bookmark"></i> Guides</a>
+                    <h4 class="other">Other</h4>
+                    <a href="#"><i class="fa-solid fa-thumbs-up"></i> Privacy Policy</a>
+                    <a href="#"><i class="fa-regular fa-eye"></i> Terms of use</a>
+                </div>
+                <div class="social">
+                    <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-github"></i></a>
+                    <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitch"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+                <div class="popular-tags flex">
+                    <h3>Popular Tags</h3>
+                    <div class="p-tags">
+                        <a href="#">#The Best</a>
+                    </div>
+                </div>
+                <div class="copy-rights flex">
+                    <p><a href="#">This blog</a> is A constructive and inclusive social network for Awesome People. With you every step of your journey.</p>
+                    <p>Made with love By DeVilHunterN1 AND McAwesome04. This Blog ©2024.</p>
+                </div>
+            </div>
                         </div>
                     </div>
             <div class="header-wrap flex">
@@ -78,6 +121,16 @@
                     <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
                     <a href="#"><i class="fa-brands fa-twitch"></i></a>
                     <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+                <div class="popular-tags flex">
+                    <h3>Popular Tags</h3>
+                    <div class="p-tags">
+                        <a href="#">#The Best</a>
+                    </div>
+                </div>
+                <div class="copy-rights flex">
+                    <p><a href="#">This blog</a> is A constructive and inclusive social network for Awesome People. With you every step of your journey.</p>
+                    <p>Made with love By DeVilHunterN1 AND McAwesome04. This Blog ©2024.</p>
                 </div>
             </div>
             <div class="posts">
